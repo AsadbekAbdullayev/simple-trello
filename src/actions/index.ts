00d36d98c0a67aa72a9bc44294d29d19779e6cb1
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-09-19 08:57:44 +0500

    Update index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,64 +1,58 @@
-import { IUserDetail } from './../reducers';
-import { ADD_LIST_TO_BOARD, FETCH_USER_DETAILS, API_IN_PROGRESS, ADD_CARD_TO_LIST, SWITCH_LISTS, SWITCH_CARDS } from './actionConstants';
+import { IUserDetail } from "../reducers";
+import {
+  ADD_LIST_TO_BOARD,
+  FETCH_USER_DETAILS,
+  API_IN_PROGRESS,
+  ADD_CARD_TO_LIST,
+  SWITCH_LISTS,
+  SWITCH_CARDS,
+} from "./actionConstants";
 
+// General Action Type
+interface Action<T, P> {
+  type: T;
+  payload: P;
+}
 
+// Helper to create action
+function createAction<T extends string, P>(type: T, payload: P): Action<T, P> {
+  return { type, payload };
+}
+
+// Actions
 export function apiInProgress(payload: boolean) {
-    return {
-        type: API_IN_PROGRESS,
-        payload
-    }
+  return createAction(API_IN_PROGRESS, payload);
 }
 
 export function fetchUserDetails(payload: IUserDetail) {
-    return {
-        type: FETCH_USER_DETAILS,
-        payload
-    }
+  return createAction(FETCH_USER_DETAILS, payload);
 }
 
 export function addListToBoard(boardId: string, listId: string, title: string) {
-    return {
-        type: ADD_LIST_TO_BOARD,
-        payload: {
-            listId,
-            title, 
-            boardId
-        }
-    }
+  return createAction(ADD_LIST_TO_BOARD, { boardId, listId, title });
 }
 
-
-export function addCardToList(listId: string, cardId: string, title: string, description: string) {
-    return {
-        type: ADD_CARD_TO_LIST,
-        payload: {
-            listId,
-            title, 
-            cardId,
-            description
-        }
-    }
+export function addCardToList(
+  listId: string,
+  cardId: string,
+  title: string,
+  description: string
+) {
+  return createAction(ADD_CARD_TO_LIST, { listId, cardId, title, description });
 }
 
-export function switchListItems(boardId: string, sourceId: string, destId: string) {
-    return ({
-        type: SWITCH_LISTS,
-        payload: {
-            boardId,
-            sourceId,
-            destId
-        }
-    });
+export function switchListItems(
+  boardId: string,
+  sourceId: string,
+  destId: string
+) {
+  return createAction(SWITCH_LISTS, { boardId, sourceId, destId });
 }
 
-
-export function switchCardItems(listId: string, sourceId: string, destId: string) {
-    return ({
-        type: SWITCH_CARDS,
-        payload: {
-            listId,
-            sourceId,
-            destId
-        }
-    });
+export function switchCardItems(
+  listId: string,
+  sourceId: string,
+  destId: string
+) {
+  return createAction(SWITCH_CARDS, { listId, sourceId, destId });
 }
